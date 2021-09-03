54518d8e058318f352001d93e58c280af8f6a594
John <scruffy403@gmail.com>
2021-09-03 13:33:41 +0100

    first commit

---
+++ home/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class HomePage(Page):
+
+    lead_text = models.CharField(
+        max_length=140,
+        blank=True,
+        help_text='Subheading text under the banner title'
+    )
+    button = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        related_name='+',
+        help_text='Select an optional page to link to',
+        on_delete=models.SET_NULL,
+    )
+    button_text = models.CharField(
+        max_length=50,
+        default='Read More',
+        blank=False,
+        help_text='Button text',
+    )
+    banner_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=False,
+        null=True,
+        related_name='+',
+        help_text='The banner background image',
+        on_delete=models.SET_NULL,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        PageChooserPanel("button"),
+        FieldPanel("button_text"),
+        ImageChooserPanel("banner_background_image"),
+    ]
---
+++ rocketman/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9io0w@9=l!@q(4x5r5i!k1x*%!2+e-1b!)it5ds(sa_ppv%g-6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
